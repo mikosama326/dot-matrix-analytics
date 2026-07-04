f0a5ba558a36a14b5ee5ecde3f648f77c82ffbbd
srija <srija326@gmail.com>
2026-07-03 19:23:33 -0700

    Configure CORS with allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
+allowed_origins = ENV.fetch("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "127.0.0.1:5173"
+    origins(*allowed_origins)
 
     resource "/api/*",
       headers: :any,
